935746b85c41aff0d24c8910d6da90f89d3365b7
Michael Wiseman <mcwiseman97@gmail.com>
2026-03-02 16:23:00 -0600

    Created First program.

---
+++ W1/firstProgram.py
@@ -0,0 +1,8 @@
+print("Hello World!\n")
+user_color = input("What is your favorite color: ")
+
+# Statement can be modified to include the f'' to allow the variable to be called in a print line using {variable_name}
+# EX: print(f"Your favorite color is: {user_color}\n")
+
+print("Your favorite color is")
+print(user_color)
