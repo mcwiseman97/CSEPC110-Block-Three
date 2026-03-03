8966ae9cdd043d817681e60870a85fe680f6f31a
Michael Wiseman <mcwiseman97@gmail.com>
2026-03-03 12:53:03 -0600

    Changed Upper() to capitalize()

--- W1/W01CleverStories.py
+++ W1/W01CleverStories.py
@@ -5,7 +5,8 @@ Date: 3/2/2026
 
 # Used \n to make a blank lines instead of print()
 # Added .upper() to emphasize the exclamation
-#
+
+
 verb = ["","",""]
 print("Please enter the following:\n")
 adjective = input("Adjective: ")
@@ -17,7 +18,7 @@ verb[2] = input("Verb: ")
 
 
 print('\nThe other day, I was really in trouble. It all started when I saw a very')
-print(f'{adjective} {animal} {verb[0]} down the hallway. "{exclamation.upper()}!" I yelled. But all')
+`print(f'{adjective} {animal} {verb[0]} down the hallway. "{exclamation.capitalize()}!" I yelled. But all')
 print(f'I could think to do was to {verb[1]} over and over. Miraculously,')
 print(f'that caused it to stop, but not before it tried to {verb[2]}')
 print('right in front of my family.\n')
